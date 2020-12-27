d4e5a475448206fc93e5cd0e7d660b6342d1f6a7
Petr Kutalek <petr@kutalek.cz>
2020-12-27 17:15:57 +0100

    Env cleanup

--- src/main.c
+++ src/main.c
@@ -1,24 +1,32 @@
-// SPDX-License-Identifier: MIT
+/*
+SPDX-License-Identifier: MIT
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-#include <stddef.h>             // for size_t
-#include <limits.h>             // for PATH_MAX
+#include <stddef.h>
+#include <limits.h>
 #include <signal.h>
 #include <regex.h>
 
 #include <errno.h>
-#include <unistd.h>             // getopt
+#include <unistd.h>
 #include <getopt.h>
+#include <sys/prctl.h>
 
 #include <zmq.h>
 #include <msgpack.h>
-#include <libipset/ipset.h>
+#include <libipset/ipset.h> /* !! version >=7 */
 
 #define min(a, b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
+
+#define LINE_SIZE 64
+#define V_DELTA_SIZE 16
+#define V_IP_SIZE 39
+#define TOPIC_SIZE 32
 
 /*
 struct app_config {
@@ -99,9 +107,9 @@ void process_list(const void *data, size_t data_size)
             for (unsigned int j = 0; j != p_list->val.via.array.size; ++j) {
                 msgpack_object *p_ip = p_list->val.via.array.ptr + j;
 
-                char v_ip[39] = { 0 };
-                memset(v_ip, 0, 39);
-                strncpy(v_ip, p_ip->via.str.ptr, min(p_ip->via.str.size, 38));
+                char v_ip[V_IP_SIZE] = { 0 };
+                memset(v_ip, 0, V_IP_SIZE);
+                strncpy(v_ip, p_ip->via.str.ptr, min(p_ip->via.str.size, V_IP_SIZE - 1));
 
                 if (regexec(&ipv4_regex, v_ip, 0, NULL, 0) != 0) {
                     fprintf(stderr, "<3>Received message does not contain IPv4, got '%s'\n", v_ip);
@@ -109,9 +117,9 @@ void process_list(const void *data, size_t data_size)
                 }
 
                 fprintf(stdout, "<7>Add %s\n", v_ip);
-                char line[64] = { 0 };
-                memset(line, 0, 64);
-                snprintf(line, 64, "add dynfw4_tmp %s -exist", v_ip);
+                char line[LINE_SIZE] = { 0 };
+                memset(line, 0, LINE_SIZE);
+                snprintf(line, LINE_SIZE, "add dynfw4_tmp %s -exist", v_ip);
                 ipset_parse_line(ipset, line);
             }
             fprintf(stdout, "<7>Swap content of dynfw4_tmp and dynfw4 lists\n");
@@ -160,20 +168,19 @@ void process_delta(const void *data, size_t data_size)
                 break;
             }
 
-            if (p_delta->val.type != MSGPACK_OBJECT_STR
-                || p_ip->val.type != MSGPACK_OBJECT_STR) {
+            if (p_delta->val.type != MSGPACK_OBJECT_STR || p_ip->val.type != MSGPACK_OBJECT_STR) {
 
                 fprintf(stderr, "<3>Received message does not contain right data types\n");
                 msgpack_object_print(stderr, unpacked.data);
                 break;
             }
 
-            char v_delta[16] = { 0 };
-            memset(v_delta, 0, 16);
-            strncpy(v_delta, p_delta->val.via.str.ptr, min(p_delta->val.via.str.size, 15));
-            char v_ip[39] = { 0 };
-            memset(v_ip, 0, 39);
-            strncpy(v_ip, p_ip->val.via.str.ptr, min(p_ip->val.via.str.size, 38));
+            char v_delta[V_DELTA_SIZE] = { 0 };
+            memset(v_delta, 0, V_DELTA_SIZE);
+            strncpy(v_delta, p_delta->val.via.str.ptr, min(p_delta->val.via.str.size, V_DELTA_SIZE - 1));
+            char v_ip[V_IP_SIZE] = { 0 };
+            memset(v_ip, 0, V_IP_SIZE);
+            strncpy(v_ip, p_ip->val.via.str.ptr, min(p_ip->val.via.str.size, V_IP_SIZE - 1));
 
             if (regexec(&ipv4_regex, v_ip, 0, NULL, 0) != 0) {
                 fprintf(stderr, "<3>Received message does not contain IPv4, got '%s'\n", v_ip);
@@ -181,9 +188,9 @@ void process_delta(const void *data, size_t data_size)
             }
 
             fprintf(stdout, "<7>%s %s\n", (strcmp(v_delta, "positive") == 0) ? "Add" : "Remove", v_ip);
-            char line[64] = { 0 };
-            memset(line, 0, 64);
-            snprintf(line, 64, "%s dynfw4 %s -exist", (strcmp(v_delta, "positive") == 0) ? "add" : "del", v_ip);
+            char line[LINE_SIZE] = { 0 };
+            memset(line, 0, LINE_SIZE);
+            snprintf(line, LINE_SIZE, "%s dynfw4 %s -exist", (strcmp(v_delta, "positive") == 0) ? "add" : "del", v_ip);
             if (ipset_parse_line(ipset, line) < 0) {
                 fprintf(stderr, "<3>Could not execute ipset statement '%s'\n", line);
             }
@@ -197,6 +204,26 @@ int main(int argc, char *argv[])
     int ret = EXIT_FAILURE;
 
     setlinebuf(stdout);
+    setlinebuf(stderr);
+
+    fprintf(stderr, "%s %s\n", "dynfwd", GIT_VERSION);
+
+    if (chdir("/proc") != 0) {
+        fprintf(stderr, "Could not chdir to /proc: %s\n", strerror(errno));
+        goto fail;
+    }
+    /*
+        none of our children will keep privs received via ambient capabilities,
+        none of our children will ever be granted more privs,
+        escape via ptrace is impossible,
+    */
+    if (prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_CLEAR_ALL, 0, 0, 0) < 0
+        || prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) < 0
+        || prctl(PR_SET_DUMPABLE, 0, 0, 0, 0) < 0) {
+
+        fprintf(stderr, "Unable to initialize secure subsystem\n");
+        goto fail;
+    }
 
     extern int optind;
     extern int opterr;
@@ -207,7 +234,7 @@ int main(int argc, char *argv[])
     while ((optc = getopt(argc, argv, ":Vh")) != -1) {
         switch (optc) {
             case 'V':
-                fprintf(stderr, "%s %s\n", argv[0], GIT_VERSION);
+                fprintf(stderr, "%s %s\n", "dynfwd", GIT_VERSION);
                 ret = EXIT_SUCCESS;
                 goto fail;
 
@@ -229,11 +256,17 @@ int main(int argc, char *argv[])
         }
     }
 
+    if (optind < argc) {
+        fprintf(stderr, "Extra argument '%s' not understood\n", argv[optind]);
+        fprintf(stderr, "For usage options run '... -h'\n");
+        goto fail;
+    }
+
     if (regcomp(&ipv4_regex, "^([0-9]{1,3}\\.){3}[0-9]{1,3}$", REG_EXTENDED | REG_NOSUB) != 0) {
         fprintf(stderr, "<3>Could not compile IPv4 regex\n");
         goto fail;
     }
-    if (regcomp(&ipv6_regex, "^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$", REG_EXTENDED | REG_NOSUB) != 0) {
+    if (regcomp(&ipv6_regex, "^([0-9a-f]{0,4}:){7}[0-9a-f]{0,4}$", REG_EXTENDED | REG_NOSUB | REG_ICASE) != 0) {
         fprintf(stderr, "<3>Could not compile IPv6 regex\n");
         goto fail;
     }
@@ -245,7 +278,7 @@ int main(int argc, char *argv[])
         goto fail;
     }
 
-    int major, minor, patch;
+    int major = 0, minor = 0, patch = 0;
     zmq_version(&major, &minor, &patch);
     fprintf(stderr, "<6>Current 0MQ version is %d.%d.%d\n", major, minor, patch);
 
@@ -253,10 +286,12 @@ int main(int argc, char *argv[])
     void *subscriber = zmq_socket(context, ZMQ_SUB);
 
     const char *server_public_key = "D=rkicUyn5AcL)>9$2Db0HIELd2F*WaS-zDTikU*";
-    char client_public_key[41];
-    char client_secret_key[41];
+    char client_public_key[41]; /* 40 + \0 */
+    char client_secret_key[41]; /* 40 + \0 */
 
     zmq_curve_keypair(client_public_key, client_secret_key);
+    client_public_key[40] = '\0';
+    client_secret_key[40] = '\0';
     zmq_setsockopt(subscriber, ZMQ_CURVE_SERVERKEY, server_public_key, 40);
     zmq_setsockopt(subscriber, ZMQ_CURVE_PUBLICKEY, client_public_key, 40);
     zmq_setsockopt(subscriber, ZMQ_CURVE_SECRETKEY, client_secret_key, 40);
@@ -266,9 +301,9 @@ int main(int argc, char *argv[])
 
     zmq_connect(subscriber, "tcp://sentinel.turris.cz:7087");
 
-    /* this is not an error, 0MQ does not take \0 string termination */
+    /* this is not an error, 0MQ does not take \0 string termination => only 11 bytes */
     zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "dynfw/delta", 11);
-    /* this is not an error, 0MQ does not take \0 string termination */
+    /* this is not an error, 0MQ does not take \0 string termination => only 10 bytes */
     zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "dynfw/list", 10);
 
     if (signal(SIGTERM, signal_handler) == SIG_ERR
@@ -279,52 +314,56 @@ int main(int argc, char *argv[])
         goto fail;
     }
 
-    zmq_pollitem_t poll[1] = {
-        { subscriber, -1, ZMQ_POLLIN, 0 }
-    };
+    zmq_pollitem_t poll[] = { {subscriber, -1, ZMQ_POLLIN, 0} };
 
     while (!request_terminate) {
         int res = zmq_poll(poll, 1, 0);
 
         if (res == 0) {
-            usleep(100 * 1000);
+            /* nothing to process */
+            /* 250 ms */
+            usleep(250 * 1000);
             continue;
         } else if (res <= 0) {
             fprintf(stderr, "<3>Error during 0MQ polling, %s\n", zmq_strerror(zmq_errno()));
-            usleep(100 * 1000);
+            /* 250 ms */
+            usleep(250 * 1000);
             continue;
         }
 
-        char topic[32] = { 0 };
+        /* received topic */
+        char topic[TOPIC_SIZE] = { 0 };
 
+        int received_size = 0;
         zmq_msg_t msg;
-        int size = 0;
-        void *data = NULL;
-        size_t data_size = 0;
+        void *msg_data = NULL;
+        size_t msg_data_size = 0;
 
+        /* topic */
         zmq_msg_init(&msg);
-        size = zmq_msg_recv(&msg, subscriber, ZMQ_DONTWAIT);
-        if (size > 0) {
-            data = zmq_msg_data(&msg);
-            data_size = zmq_msg_size(&msg);
-            memset(topic, 0, 32);
-            strncpy(topic, data, min(data_size, 31));
+        received_size = zmq_msg_recv(&msg, subscriber, ZMQ_DONTWAIT);
+        if (received_size > 0) {
+            msg_data = zmq_msg_data(&msg);
+            msg_data_size = zmq_msg_size(&msg);
+            memset(topic, 0, TOPIC_SIZE);
+            strncpy(topic, msg_data, min(msg_data_size, TOPIC_SIZE - 1));
         }
         zmq_msg_close(&msg);
 
+        /* payload */
         zmq_msg_init(&msg);
-        size = zmq_msg_recv(&msg, subscriber, ZMQ_DONTWAIT);
-        if (size > 0) {
-            data = zmq_msg_data(&msg);
-            data_size = zmq_msg_size(&msg);
+        received_size = zmq_msg_recv(&msg, subscriber, ZMQ_DONTWAIT);
+        if (received_size > 0) {
+            msg_data = zmq_msg_data(&msg);
+            msg_data_size = zmq_msg_size(&msg);
             if (strcmp("dynfw/delta", topic) == 0) {
-                process_delta(data, data_size);
+                process_delta(msg_data, msg_data_size);
             }
             if (strcmp("dynfw/list", topic) == 0) {
                 fprintf(stderr, "<6>Switching %s dynfw/list\n", "off");
                 /* this is not an error, 0MQ does not take \0 string termination */
                 zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, "dynfw/list", 10);
-                process_list(data, data_size);
+                process_list(msg_data, msg_data_size);
             }
         }
         zmq_msg_close(&msg);
